cc6a5d675568d5f53b080430e1c9646069a31d71
Realharshagarwal <Realharshagarwal@gmail.com>
2025-06-27 19:01:58 +0530

    Created initial Files for RouterLibrary Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>adfh</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/BrowserRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BrowserRouter(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default BrowserRouter;
